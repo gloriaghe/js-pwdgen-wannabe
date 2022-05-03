30ab7e22b9aa774212ee974118804749af0b1c49
Gloria <gloghe@yahoo.it>
2022-05-03 16:10:01 +0200

    punti e virgola

--- js/script.js
+++ js/script.js
@@ -6,16 +6,16 @@ console.log(tuoNome);
 
 //chiedi cognome
 
-const tuoCognome = prompt("Qual è il tuo cognome?")
+const tuoCognome = prompt("Qual è il tuo cognome?");
 
 console.log(tuoCognome);
 
 //chiedi colore preferito
 
-const colorePreferito = prompt("Qual'è il tuo colore preferito?")
+const colorePreferito = prompt("Qual'è il tuo colore preferito?");
 
 console.log(colorePreferito);
 
 //genera password "nomecognomecolorepreferito21"
 
-document.getElementById("my-password").innerHTML= `${tuoNome}${tuoCognome}${colorePreferito}21` 
+document.getElementById("my-password").innerHTML= `${tuoNome}${tuoCognome}${colorePreferito}21` ;
