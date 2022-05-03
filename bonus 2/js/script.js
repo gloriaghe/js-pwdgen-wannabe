f92e8dc32205fe07d59503c7e2b57765ab374be4
Gloria <gloghe@yahoo.it>
2022-05-03 17:37:35 +0200

    bonus 2

--- bonus 2/js/script.js
+++ bonus 2/js/script.js
@@ -1,32 +1,31 @@
 // nome iniziale
 
-const tuoNome = prompt("Ciao, come ti chiami?");
+const tuoNome = prompt("Ciao, sono il tuo generatore automatico di password del mondo di Harry Potter....Cominciamo???? Intanto dimmi come ti chiami!");
 
 console.log(tuoNome);
 
 document.getElementById("ilTuoNome").innerHTML= `${tuoNome}`
 
 
-function myFunction() {
 // // chiedi personaggio
 
-const personaggio = prompt("Qual'è il tuo personaggio preferito?");
+const personaggio = prompt("Ora la prima domanda... Qual'è il tuo personaggio preferito?");
 
 console.log(personaggio);
 
 // //chiedi animale magico
 
-const animale = prompt("Qual è il tuo animale magico?");
+const animale = prompt("E il tuo animale magico qual è?");
 
 console.log(animale);
 
 // //chiedi numero preferito
 
-const numeroPreferito = prompt("Qual'è il tuo numero preferito?");
+const numeroPreferito = prompt("Per finire dimmi il tuo numero preferito!");
 
 console.log(numeroPreferito);
-};
+
 
 // //genera password "nomecognomecolorepreferito21"
 
-document.getElementById("MYpassword").innerHTML= `La tua password è ${personaggio}${animale}${numeroPreferito}21` ;
+document.getElementById("MYpassword").innerHTML= `La tua password è: ${personaggio}${animale}${numeroPreferito}21` ;
